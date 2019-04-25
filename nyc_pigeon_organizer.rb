dad3c52fcab3397bfaab83630d5d7ceae20687c6
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-25 21:53:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,8 @@ def nyc_pigeon_organizer(data)
   data.each do |thing, details|
     details.each do |subthing, names|
       names.each do |name|
-        new_hash[name] = {}
-        new_hash[name][thing] = []
+        new_hash[name] ||= {}
+        new_hash[name][thing] ||= []
         new_hash[name][thing] << subthing.to_s
       end
     end
